a9a7437da1c4283d2fc2b849d2b120d003d492fb
Nemanja501 <nemanjavulin5@gmail.com>
2023-11-07 19:55:56 +0100

    zadatak 3

--- comments.php
+++ comments.php
@@ -15,21 +15,11 @@
 
     <!-- Custom styles for this template -->
     <link href="styles/blog.css" rel="stylesheet">
-    <link href="styles/styles.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-    <header>
-        <div class="blog-masthead">
-            <div class="container">
-                <nav class="nav">
-                    <a class="nav-link active" href="#">Home</a>
-                    <a class="nav-link" href="#">New features</a>
-                    <a class="nav-link" href="#">Press</a>
-                    <a class="nav-link" href="#">New hires</a>
-                    <a class="nav-link" href="#">About</a>
-                </nav>
-            </div>
-        </div>
-    </header>
+    <?php 
+    include "header.php";
+    include "sidebar.php";
+    include "footer.php";    
+    ?>
 </body>
-</html>
--- posts.php
+++ posts.php
@@ -15,6 +15,7 @@
 
     <!-- Custom styles for this template -->
     <link href="styles/blog.css" rel="stylesheet">
+    <link href="styles/styles.css" type="text/css" rel="stylesheet">
 </head>
 
 <body>
@@ -35,7 +36,7 @@
             <div class="col-sm-8 blog-main">
 
                 <div class="blog-post">
-                    <h2 class="blog-post-title">Sample blog post</h2>
+                    <a href="single_post.php"><h2 class="blog-post-title">Sample blog post</h2></a>
                     <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
 
                     <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
@@ -68,7 +69,7 @@
                 </div><!-- /.blog-post -->
 
                 <div class="blog-post">
-                    <h2 class="blog-post-title">Another blog post</h2>
+                    <a href="single_post.php"><h2 class="blog-post-title">Another blog post</h2></a>
                     <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
 
                     <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
@@ -80,7 +81,7 @@
                 </div><!-- /.blog-post -->
 
                 <div class="blog-post">
-                    <h2 class="blog-post-title">New feature</h2>
+                    <a href="single_post.php"><h2 class="blog-post-title">New feature</h2></a>
                     <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
 
                     <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
